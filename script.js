113fe9542d98c0ef72bec704a7a82bdae54d5b9f
Cholo0022 <andres.bozzani@gmail.com>
2022-02-22 16:03:39 -0300

    Menu principal, presentacion, skills y hobbies terminado, faltan detalles

---
+++ script.js
@@ -0,0 +1,11 @@
+linkSobreMi.addEventListener("click", function () {
+  sobreMi.scrollIntoView();
+});
+
+linkSkills.addEventListener("click", function () {
+  skills.scrollIntoView();
+});
+
+linkHobbies.addEventListener("click", function () {
+  hobbies.scrollIntoView();
+});
