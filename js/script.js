3f65e826902f9059a696d96edba06446b494deef
Cholo0022 <andres.bozzani@gmail.com>
2022-03-16 23:24:56 -0300

    Validaciones terminados, faltan detalles

--- js/script.js
+++ js/script.js
@@ -1,63 +1,123 @@
-let botonEnviar = document.getElementById("botonEnviar");
+//Valida campo Nombre
+let nombre = document.querySelector("#nombre");
+let errorNombre = document.querySelector(".errorNombre");
 
-botonEnviar.addEventListener("click", function () {
-  let inputNombre = document.querySelector("#nombre");
-  let inputEmail = document.querySelector("#email");
-  let expresion =
-    /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
-  let inputAsunto = document.querySelector("#asunto");
-  let inputMensaje = document.querySelector("#mensaje");
-  console.log(inputNombre.value.length);
-  if (inputNombre.value === "") {
-    let errorNombre = document.querySelector(".errorNombre");
-    errorNombre.textContent = "Campo obligatorio";
-    errorNombre.style.color = "red";
-    document.querySelector("#nombre").focus();
-  }
-
-  if (inputNombre.value.length > 50) {
-    let errorNombre = document.querySelector(".errorNombre");
-    errorNombre.textContent = "Solo se permite hasta 50 caracteres";
-    errorNombre.style.color = "red";
-    document.querySelector("#nombre").value = "";
-    document.querySelector("#nombre").focus();
-  }
-
-  if (inputEmail.value === "") {
-    let errorEmail = document.querySelector(".errorEmail");
-    errorEmail.textContent = "Campo obligatorio";
-    errorEmail.style.color = "red";
-    document.querySelector("#email").value = "";
-    document.querySelector("#email").focus();
-  } else {
-    if (!expresion.exec(inputEmail.value)) {
-      let errorEmail = document.querySelector(".errorEmail");
-      errorEmail.textContent = "Email incorrecto";
-      errorEmail.style.color = "red";
-      document.querySelector("#email").value = "";
-      document.querySelector("#email").focus();
+nombre.addEventListener(
+  "blur",
+  function (event) {
+    let nombre = event.target.value;
+    event.target.style.background = "";
+    if (nombre === "") {
+      errorNombre.textContent = "Campo obligatorio";
+      errorNombre.style.color = "red";
     }
-  }
-  if (inputAsunto.value === "") {
-    let errorAsunto = document.querySelector(".errorAsunto");
-    errorAsunto.textContent = "Campo obligatorio";
-    errorAsunto.style.color = "red";
-    document.querySelector("#asunto").value = "";
-    document.querySelector("#asunto").focus();
-  }
+    if (nombre.length > 50) {
+      let errorNombre = document.querySelector(".errorNombre");
+      errorNombre.textContent = "Solo se permite hasta 50 caracteres";
+      errorNombre.style.color = "red";
+    }
+  },
+  true
+);
 
-  if (inputMensaje.value === "") {
-    let errorMensaje = document.querySelector(".errorMensaje");
-    errorMensaje.textContent = "Campo obligatorio";
-    errorMensaje.style.color = "red";
-    document.querySelector("#mensaje").focus();
-  }
+//Cuando el foco está en el campo nombre, se pinta rosado y se borra el mensaje de error
+nombre.addEventListener(
+  "focus",
+  function (event) {
+    event.target.style.background = "pink";
+    errorNombre.textContent = "";
+  },
+  true
+);
 
-  if (inputMensaje.value.length > 300) {
-    let errorMensaje = document.querySelector(".errorMensaje");
-    errorMensaje.textContent = "Solo se permite hasta 300 caracteres";
-    errorMensaje.style.color = "red";
-    document.querySelector("#mensaje").value = "";
-    document.querySelector("#mensaje").focus();
-  }
-});
+//Valida campo Email
+let email = document.querySelector("#email");
+let expresion =
+  /^(([^<>()\[\]\\.,;:\s@”]+(\.[^<>()\[\]\\.,;:\s@”]+)*)|(“.+”))@((\[[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}\.[0–9]{1,3}])|(([a-zA-Z\-0–9]+\.)+[a-zA-Z]{2,}))$/;
+let errorEmail = document.querySelector(".errorEmail");
+
+email.addEventListener(
+  "blur",
+  function (event) {
+    event.target.style.background = "";
+    let email = event.target.value;
+    if (email === "") {
+      errorEmail.textContent = "Campo obligatorio";
+      errorEmail.style.color = "red";
+    } else {
+      if (!expresion.exec(email)) {
+        errorEmail.textContent = "Email incorrecto";
+        errorEmail.style.color = "red";
+      }
+    }
+  },
+  true
+);
+
+//Cuando el foco está en el campo email, se pinta rosado y se borra el mensaje de error
+email.addEventListener(
+  "focus",
+  function (event) {
+    event.target.style.background = "pink";
+    errorEmail.textContent = "";
+  },
+  true
+);
+
+//Valida campo Asunto
+let asunto = document.querySelector("#asunto");
+let errorAsunto = document.querySelector(".errorAsunto");
+
+asunto.addEventListener(
+  "blur",
+  function (event) {
+    let asunto = event.target.value;
+    event.target.style.background = "";
+    if (asunto === "") {
+      errorAsunto.textContent = "Campo obligatorio";
+      errorAsunto.style.color = "red";
+    }
+  },
+  true
+);
+
+//Cuando el foco está en el campo asunto, se pinta rosado y se borra el mensaje de error
+asunto.addEventListener(
+  "focus",
+  function (event) {
+    event.target.style.background = "pink";
+    errorAsunto.textContent = "";
+  },
+  true
+);
+
+//Valida campo Mensaje
+let mensaje = document.querySelector("#mensaje");
+let errorMensaje = document.querySelector(".errorMensaje");
+
+mensaje.addEventListener(
+  "blur",
+  function (event) {
+    let mensaje = event.target.value;
+    event.target.style.background = "";
+    if (mensaje === "") {
+      errorMensaje.textContent = "Campo obligatorio";
+      errorMensaje.style.color = "red";
+    }
+    if (mensaje.length > 300) {
+      errorMensaje.textContent = "Solo se permite hasta 300 caracteres";
+      errorMensaje.style.color = "red";
+    }
+  },
+  true
+);
+
+//Cuando el foco está en el campo mensaje, se pinta rosado y se borra el mensaje de error
+mensaje.addEventListener(
+  "focus",
+  function (event) {
+    event.target.style.background = "pink";
+    errorMensaje.textContent = "";
+  },
+  true
+);
